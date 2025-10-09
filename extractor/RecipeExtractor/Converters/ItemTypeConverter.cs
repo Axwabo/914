c50d5213152f40e51c564fbfc4030f4a2411b93c
Axwabo <axwabo@gmail.com>
2025-10-09 18:55:38 +0200

    ammo names

--- extractor/RecipeExtractor/Converters/ItemTypeConverter.cs
+++ extractor/RecipeExtractor/Converters/ItemTypeConverter.cs
@@ -6,9 +6,19 @@ public sealed class ItemTypeConverter : WriteOnlyJsonConverter<ItemType>
 {
 
     public override void Write(Utf8JsonWriter writer, ItemType value, JsonSerializerOptions options)
-        => writer.WriteStringValue(value.GetName());
+        => writer.WriteStringValue(Translate(value));
 
     public override void WriteAsPropertyName(Utf8JsonWriter writer, ItemType value, JsonSerializerOptions options)
-        => writer.WritePropertyName(value.GetName());
+        => writer.WritePropertyName(Translate(value));
+
+    private static string Translate(ItemType value) => value switch
+    {
+        ItemType.Ammo12gauge => "12/70 Buckshot Ammo",
+        ItemType.Ammo556x45 => "5.56x45mm Ammo",
+        ItemType.Ammo44cal => ".44 Mag Ammo",
+        ItemType.Ammo762x39 => "7.62x39mm Ammo",
+        ItemType.Ammo9x19 => "9x19mm Ammo",
+        _ => value.GetName()
+    };
 
 }
