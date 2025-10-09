54d26c2eef5cb00fb26313496c94b5aeea431178
Axwabo <axwabo@gmail.com>
2025-10-09 12:05:36 +0200

    attempt KnobSettingList refactor

--- extractor/RecipeExtractor/KnobSettingList.cs
+++ extractor/RecipeExtractor/KnobSettingList.cs
@@ -1,5 +1,31 @@
-﻿using Scp914;
+﻿using System.Text;
+using Scp914;
 
 namespace RecipeExtractor;
 
-public sealed record KnobSettingList(params Scp914KnobSetting[] Settings);
+public record struct KnobSettingList(bool Rough, bool Coarse, bool OneToOne, bool Fine, bool VeryFine)
+{
+
+    public static KnobSettingList From(params IEnumerable<Scp914KnobSetting> settings)
+    {
+        var list = new KnobSettingList();
+        foreach (var setting in settings)
+        {
+            list.Rough |= setting == Scp914KnobSetting.Rough;
+            list.Coarse |= setting == Scp914KnobSetting.Coarse;
+            list.OneToOne |= setting == Scp914KnobSetting.OneToOne;
+            list.Fine |= setting == Scp914KnobSetting.Fine;
+            list.VeryFine |= setting == Scp914KnobSetting.VeryFine;
+        }
+
+        return list;
+    }
+
+    public static implicit operator KnobSettingList(Scp914KnobSetting setting) => From(setting);
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+    }
+
+}
