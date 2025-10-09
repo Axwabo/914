cf438d281467036cf86a1114e434e71917ac8b12
Axwabo <axwabo@gmail.com>
2025-10-09 19:29:14 +0200

    item images + basic types

---
+++ src/types/outputs.ts
@@ -0,0 +1,20 @@
+﻿import type { Item } from "./item.ts";
+
+export type OutputKind = Extract<Output, { type: string }>["type"];
+
+export type Output = { chance: number } & (
+    ItemOutput
+    | KindOutput<"destroy">
+    | KindOutput<"randomize">
+    | KindOutput<"recharge">
+    | KindOutput<"break">
+    | KindOutput<"nothing">);
+
+interface ItemOutput {
+    kind: "item";
+    items: Item[];
+}
+
+interface KindOutput<T extends string> {
+    kind: T;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     },
 
     workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        globPatterns: ['**/*.{js,css,html,svg,png,ico,webp,json}'],
       cleanupOutdatedCaches: true,
       clientsClaim: true,
     },
