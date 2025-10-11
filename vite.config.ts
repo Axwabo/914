4194cc6527d74e8d9bec1982158bb8efb3a04a4e
Axwabo <axwabo@gmail.com>
2025-10-11 16:54:33 +0200

    change base URL

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
-const base = "/914/";
+const base = "/914-Remake/";
 
 // https://vitejs.dev/config/
 export default defineConfig({
