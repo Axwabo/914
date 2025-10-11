60011b307d4a4adf1a666258b1a16929ee350b11
Axwabo <axwabo@gmail.com>
2025-10-11 17:01:37 +0200

    adjust About.vue

--- src/components/dialog/About.vue
+++ src/components/dialog/About.vue
@@ -1,7 +1,9 @@
 ﻿<template>
     <p>This website is not affiliated with Northwood Studios.<br>SCP: Secret Laboratory is a free-to-play multiplayer game developed by Northwood Studios.
-    </p><a href="https://scpslgame.com" target="SCP: Secret Laboratory">Visit the official site for more information</a>
+    </p>
+    <a href="https://scpslgame.com" target="SCP: Secret Laboratory">Visit the official site for more information</a>
     <p>This app lets you view the SCP-914 recipes in SCP:SL.<br>All recipes were extracted from the server code and files.
     </p>
+    <p>Currently, only output viewing is supported. Click on an item to display the dialog.</p>
     <p>Found an issue? Report it on <a>GitHub</a></p>
 </template>
