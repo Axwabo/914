4c6d233663ecaf8747a67d81e0b03c8dcc8eb0e5
Axwabo <axwabo@gmail.com>
2025-10-16 10:50:05 +0200

    remove context menu + begin item selection

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import { createPinia } from "pinia";
-import useStore from "./store.ts";
 
 createApp(App).use(createPinia()).mount("#app");
-
-const { openContextMenu } = useStore();
-
-window.addEventListener("click", e => {
-    if (!(e.target as HTMLElement)?.matches("dialog.context-menu, dialog.context-menu *"))
-        openContextMenu(null);
-});
--- src/store.ts
+++ src/store.ts
@@ -1,27 +1,43 @@
 ﻿import { defineStore } from "pinia";
 import type { DialogType, ItemPage } from "./types/dialogs.ts";
 import type { ItemType } from "./types/item.ts";
+import type { Selecting } from "./types/selection.ts";
 
 interface State {
     dialogType: DialogType;
     itemPage: ItemPage;
     item: ItemType | null;
+    from: ItemType | null;
+    to: ItemType | null;
+    selecting: Selecting;
 }
 
 const store = defineStore("dialogs", {
-    state: (): State => ({ dialogType: "none", itemPage: "outputs", item: null }),
+    state: (): State => ({
+        dialogType: "none",
+        itemPage: "outputs",
+        item: null,
+        from: null,
+        to: null,
+        selecting: "none"
+    }),
     actions: {
         showModal(type: DialogType) {
             this.dialogType = type;
         },
-        openContextMenu(menu: HTMLDialogElement | null) {
-            const current = document.querySelector("dialog.context-menu[open]") as HTMLDialogElement;
-            if (current === menu)
-                return;
-            current?.close();
-            menu?.show();
-        },
         interact(type: ItemType) {
+            switch (this.selecting) {
+                case "none":
+                    break;
+                case "from":
+                    this.from = type;
+                    this.selecting = "none";
+                    return;
+                case "to":
+                    this.to = type;
+                    this.selecting = "none";
+                    return;
+            }
             if (this.dialogType === "none")
                 this.itemPage = "outputs";
             this.dialogType = "item";
