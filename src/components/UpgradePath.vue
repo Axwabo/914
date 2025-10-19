62305788eb8821c789853b34ef6b6d7de306212c
Axwabo <axwabo@gmail.com>
2025-10-19 12:38:16 +0200

    partial upgrade step tracing

--- src/components/UpgradePath.vue
+++ src/components/UpgradePath.vue
@@ -1,6 +1,7 @@
 ﻿<script setup lang="ts">
 import useStore from "../store.ts";
 import { findPath } from "../utils/upgrading.ts";
+import Scp914Item from "./Scp914Item.vue";
 
 const { from, to } = useStore();
 
@@ -9,6 +10,12 @@ const path = findPath(from!, to!);
 
 <template>
     <span v-if="path===null">No connection</span>
+    <template v-else>
+        <template v-for="(node, index) in path">
+            <Scp914Item v-if="index !== 0" :type="node.from" small disabled />
+            <span>→ {{ node.mode }} →</span>
+        </template>
+    </template>
 </template>
 
 <style scoped>
--- src/utils/upgrading.ts
+++ src/utils/upgrading.ts
@@ -12,10 +12,18 @@ interface UpgradeNode {
 type UpgradePath = UpgradeNode[];
 
 export function findPath(from: ItemType, to: ItemType): UpgradePath | null {
-    const paths = allPaths[to];
-    let step = paths[from];
-    if (!step || isFinite(step.distance))
-        return null;
-    // TODO: trace steps
-    return [];
+    const paths = allPaths[from];
+    let step = paths[to];
+    const stack: UpgradePath = [];
+    let previous = to;
+    while (step?.predecessor !== from) {
+        if (!step || !step.predecessor || !isFinite(step.distance))
+            return null;
+        stack.push({ from: step.predecessor, to: previous, mode: "1:1", chance: 1 });
+        previous = step.predecessor;
+        step = paths[step.predecessor];
+    }
+    stack.push({ from, to: previous, mode: "1:1", chance: 1 });
+    stack.reverse();
+    return stack;
 }
