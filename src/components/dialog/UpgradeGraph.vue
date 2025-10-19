6bfb0ea9a41020d124236f84d3da19c78c03f671
Axwabo <axwabo@gmail.com>
2025-10-19 09:35:32 +0200

    dagre graph layout

--- src/components/dialog/UpgradeGraph.vue
+++ src/components/dialog/UpgradeGraph.vue
@@ -1,31 +1,7 @@
 ﻿<script setup lang="ts">
-import { type Edge, VueFlow } from "@vue-flow/core";
-import { ref } from "vue";
-import { recipes } from "../../cache.ts";
-import { itemTypes, keys } from "../../utils/keys.ts";
+import { VueFlow } from "@vue-flow/core";
+import { edges, nodes } from "../../utils/graph.ts";
 import Close from "./Close.vue";
-
-const gridItems = Math.ceil(Math.sqrt(itemTypes.length));
-
-const nodes = ref(itemTypes.map((type, index) => ({
-    id: type,
-    position: {
-        x: 300 * Math.floor(index / gridItems),
-        y: 300 * (index % gridItems)
-    },
-    data: { label: type }
-})));
-
-const edges: Edge[] = keys(recipes).map(type => {
-    const recipe = recipes[type];
-    return keys(recipe).map(mode => recipe[mode]!.filter(e => e.kind === "item").map(output => output.items.map((item): Edge => ({
-        id: `${type} -> ${mode} -> ${item.type}`,
-        source: type,
-        target: item.type,
-        label: mode,
-        markerEnd: "arrow"
-    }))));
-}).flat(3);
 </script>
 
 <template>
---
+++ src/utils/graph.ts
@@ -0,0 +1,50 @@
+﻿import { graphlib, layout } from "@dagrejs/dagre";
+import type { Edge, Node } from "@vue-flow/core";
+import { recipes } from "../cache.ts";
+import { itemTypes, keys } from "./keys.ts";
+
+const zeroZero = { x: 0, y: 0 };
+export const nodes = itemTypes.map((type): Node<any> => ({
+    id: type,
+    position: zeroZero,
+    data: { label: type }
+}));
+
+export const edges: Edge[] = keys(recipes).map(type => {
+    const recipe = recipes[type];
+    return keys(recipe).map(mode => recipe[mode]!.filter(e => e.kind === "item").map(output => output.items.map((item): Edge => ({
+        id: `${type} -> ${mode} -> ${item.type}`,
+        source: type,
+        target: item.type,
+        label: mode,
+        markerEnd: "arrow"
+    }))));
+}).flat(3);
+
+export const graph = createGraph();
+
+applyLayout();
+
+function createGraph(): graphlib.Graph {
+    const graph = new graphlib.Graph();
+    graph.setDefaultEdgeLabel(() => ({}));
+    graph.setGraph({ rankdir: "TB" });
+    for (const node of nodes)
+        graph.setNode(node.id, { width: 100, height: 100 });
+    const usedIDs = new Set<string>();
+    for (const edge of edges) {
+        if (edge.source === edge.target || usedIDs.has(edge.id))
+            continue;
+        usedIDs.add(edge.id);
+        graph.setEdge(edge.source, edge.target);
+    }
+    layout(graph);
+    return graph;
+}
+
+function applyLayout() {
+    for (const node of nodes) {
+        const positioned = graph.node(node.id)!;
+        node.position = { x: positioned.x, y: positioned.y };
+    }
+}
