0730e2fec7fe69b602726b355f02933ef69484e5
Axwabo <axwabo@gmail.com>
2025-10-19 18:57:35 +0200

    extract AmmoExchangeCalculator

---
+++ src/components/dialog/AmmoExchangeCalculator.vue
@@ -0,0 +1,58 @@
+﻿<script setup lang="ts">
+import { computed, ref } from "vue";
+import { items } from "../../cache.ts";
+import type { Ammo, ItemType } from "../../types/item.ts";
+import { exchangeAmmo } from "../../utils/convert.ts";
+import { ammo } from "../../utils/keys.ts";
+
+const input = ref<ItemType>(ammo[0]);
+const output = ref<ItemType>(ammo[1]);
+
+const amount = ref(1);
+
+const converted = computed(() => exchangeAmmo(
+    (items[input.value] as Ammo).unitPrice,
+    (items[output.value] as Ammo).unitPrice,
+    amount.value
+));
+</script>
+
+<template>
+    <h3>Calculator</h3>
+    <div class="inputs">
+        <label for="input">Input:</label>
+        <select v-model="input" id="input">
+            <option v-for="type in ammo">{{ type }}</option>
+        </select>
+        <label for="output">Output:</label>
+        <select v-model="output" id="output">
+            <option v-for="type in ammo">{{ type }}</option>
+        </select>
+        <label for="amount">Amount:</label>
+        <input type="number" v-model="amount" id="amount" min="1" />
+    </div>
+    Exchanged: <span class="number">{{ converted.exchanged }}</span>
+    <br>
+    Remaining: <span class="number">{{ converted.remaining }}</span>
+</template>
+
+<style scoped>
+h3 {
+    margin: 0;
+}
+
+.inputs {
+    justify-self: center;
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 0.5rem;
+    text-align: right;
+    max-width: 20rem;
+}
+
+.number {
+    display: inline-block;
+    min-width: 2em;
+    text-align: left;
+}
+</style>
--- src/components/dialog/UnitPrices.vue
+++ src/components/dialog/UnitPrices.vue
@@ -1,81 +1,56 @@
 ﻿<script setup lang="ts">
-import { computed, ref } from "vue";
 import { items } from "../../cache.ts";
-import type { Ammo, ItemType } from "../../types/item.ts";
-import { exchangeAmmo } from "../../utils/convert.ts";
+import type { Ammo } from "../../types/item.ts";
 import { ammo } from "../../utils/keys.ts";
+import AmmoExchangeCalculator from "./AmmoExchangeCalculator.vue";
 import Close from "./Close.vue";
-
-const input = ref<ItemType>(ammo[0]);
-const output = ref<ItemType>(ammo[1]);
-
-const amount = ref(1);
-
-const converted = computed(() => exchangeAmmo(
-    (items[input.value] as Ammo).unitPrice,
-    (items[output.value] as Ammo).unitPrice,
-    amount.value
-));
 </script>
 
 <template>
     <Close />
     <h2>Unit Prices</h2>
-    <p>
+    <div class="scroller"><p>
         Ammo upgrading works differently compared to other conversions.
         <br>
         Per <b class="input">input</b> ammo, you get
         <b class="output">output</b> number of exchanged rounds, and the remaining stays as input ammo.
     </p>
-    <code>
-        exchanged = Math.floor(amount * (inputPrice / outputPrice))
-        <br>
-        remaining = Math.ceil(amount - exchanged * (outputPrice / inputPrice))
-    </code>
-    <div class="table-container">
-        <table>
-            <thead>
-            <tr>
-                <th>Input</th>
-                <th v-for="type in ammo" class="output">{{ type }}</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="input in ammo">
-                <td class="input">{{ input }}</td>
-                <td v-for="output in ammo">
-                    <span class="input">{{ (items[input] as Ammo).unitPrice }}</span>
-                    :
-                    <span class="output">{{ (items[output] as Ammo).unitPrice }}</span>
-                </td>
-            </tr>
-            </tbody>
-        </table>
-    </div>
-    <div class="calculator">
-        <h3>Calculator</h3>
-        <label for="input">Input: </label>
-        <select v-model="input" id="input">
-            <option v-for="type in ammo">{{ type }}</option>
-        </select>
-        <br>
-        <label for="output">Output: </label>
-        <select v-model="output" id="output">
-            <option v-for="type in ammo">{{ type }}</option>
-        </select>
-        <br>
-        <label for="amount">Amount: </label>
-        <input type="number" v-model="amount" id="amount" min="1" />
-        <br>
-        Exchanged: {{ converted.exchanged }}
-        <br>
-        Remaining: {{ converted.remaining }}
+        <code>
+            exchanged = Math.floor(amount * (inputPrice / outputPrice))
+            <br>
+            remaining = Math.ceil(amount - exchanged * (outputPrice / inputPrice))
+        </code>
+        <div class="table-container">
+            <table>
+                <thead>
+                <tr>
+                    <th>Input</th>
+                    <th v-for="type in ammo" class="output">{{ type }}</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="input in ammo">
+                    <td class="input">{{ input }}</td>
+                    <td v-for="output in ammo">
+                        <span class="input">{{ (items[input] as Ammo).unitPrice }}</span>
+                        :
+                        <span class="output">{{ (items[output] as Ammo).unitPrice }}</span>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <AmmoExchangeCalculator />
     </div>
 </template>
 
 <style scoped>
-h2, h3 {
+h2 {
     margin: 0;
+}
+
+.scroller {
+    overflow-y: auto;
 }
 
 .table-container {
